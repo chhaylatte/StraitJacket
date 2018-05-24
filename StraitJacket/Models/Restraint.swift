c5926a35d548ad4c107f1fe2d3a4a504d5ce1b28
Danny Chhay <danchhay@gmail.com>
2018-05-24 15:58:13 -0700

    Refactor Edges to file

--- StraitJacket/Models/Restraint.swift
+++ StraitJacket/Models/Restraint.swift
@@ -45,23 +45,6 @@ public class Restraint<T: UIView> {
     }
 }
 
-public struct Edges: OptionSet {
-    public let rawValue: Int
-    
-    public init(rawValue: Int) {
-        self.rawValue = rawValue
-    }
-    
-    public static let top = Edges(rawValue: 1 << 0)
-    public static let bottom = Edges(rawValue: 1 << 1)
-    public static let left = Edges(rawValue: 1 << 2)
-    public static let right = Edges(rawValue: 1 << 3)
-    
-    public static let all: Edges = [.top, .bottom, .left, .right]
-    public static let vertical: Edges = [.top, .bottom]
-    public static let horizontal: Edges = [.left, .right]
-}
-
 extension Edges: Hashable {
     public var hashValue: Int {
         return rawValue
