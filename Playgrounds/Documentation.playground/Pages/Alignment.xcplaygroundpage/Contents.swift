46546a2cc7b49c9cd9eb179440fc7d58eba89844
Danny Chhay <danchhay@gmail.com>
2018-09-27 17:50:34 -0700

    Clean up, tweak comments

--- Playgrounds/Documentation.playground/Pages/Alignment.xcplaygroundpage/Contents.swift
+++ Playgrounds/Documentation.playground/Pages/Alignment.xcplaygroundpage/Contents.swift
@@ -4,41 +4,55 @@ import StraitJacket
 /*:
  # Alignment
  
- Use `func alignItems(_ views: [RestraintTargetable], to alignment: Set<Alignment>, of target: RestraintTargetable)` on `Restraint` to align many views to the same target.
+ ## Aligning to the Root View
  
- Use `func alignItems(_ views: [RestraintTargetable], to alignment: Set<Alignment>)` to align to the root view.
+ The `Alignment` enum describes various possible ways to align items including the sides and centers.
+ 
+ To align many views to the root view:
+ 
+ `func alignItems(_ views: [RestraintTargetable], to alignment: Set<Alignment>)`
  */
-
-let blackSubview = UIView()
+// Set up the views
+var blackSubview = UIView()
 blackSubview.backgroundColor = .black
 
 let graySubview = UIView()
 graySubview.backgroundColor = .gray
 
+var aView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+aView.backgroundColor = .white
+
+// Align items with a `Restraint`
+var aRestraint = Restraint(aView, items: [blackSubview, graySubview])
+    .alignItems([blackSubview, graySubview], to: [.centerX, .centerY])
+aRestraint.activate()
+
+// Create size constraints with anchors since we're learning about Alignment only right now.
 let sizeConstraints = [
     blackSubview.widthAnchor.constraint(equalToConstant: 200),
     blackSubview.heightAnchor.constraint(equalToConstant: 200),
     graySubview.widthAnchor.constraint(equalToConstant: 100),
     graySubview.heightAnchor.constraint(equalToConstant: 100),
 ]
-let grayWidthConstraint = sizeConstraints[2]
-grayWidthConstraint.priority = .defaultHigh
+
 NSLayoutConstraint.activate(sizeConstraints)
 
-var aView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
-aView.backgroundColor = .white
-
-//: Align many items to the same target with the same alignment.
-
-var aRestraint = Restraint(aView, items: [blackSubview, graySubview])
-    .alignItems([blackSubview, graySubview], to: [.centerX, .centerY])
-aRestraint.activate()
+// This will be used later
+var grayWidthConstraint = sizeConstraints[2]
+grayWidthConstraint.priority = .defaultHigh
 
 aView
 
 /*:
+ ## Aligning to a Target
+ 
+ To align many views to the same target:
+ 
+ `func alignItems(_ views: [RestraintTargetable], to alignment: Set<Alignment>, of target: RestraintTargetable)`
+ 
  `RestraintTargettable` can be either a kind of UIView or a UILayoutGuide.
  */
+
 aView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
 aView.backgroundColor = .white
 
@@ -50,12 +64,6 @@ aRestraint.activate()
 
 aView
 
-/*:
- The `Alignment` enum describes various possible ways to align items including the sides and centers.
- 
- The soft enums: .softTop, .softBottom, .softLeft, .softRight are used for creating inequality constraints to staty within the bounds of the target.
- */
-
 aView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
 aView.backgroundColor = .white
 
@@ -64,23 +72,42 @@ aRestraint = Restraint(aView, items: [blackSubview, graySubview])
     .alignItems([graySubview], to: [.centerX, .centerY], of: blackSubview)
 aRestraint.activate()
 
-let graySubviewBoundaryRestraint = Restraint(aView)
-    .alignItems([graySubview], to: [.softLeft, .softRight], of: blackSubview)
+/*:
+ ## Soft Aligning
+ 
+ Alignment also define "soft" versions of alignment.  Soft alignment is used for creating inequality constraints to stay within the bounds of the target.
+ */
+
 aView
 
+// The gray view's width is set to be larger than the black view to simulate large content.
 grayWidthConstraint.constant = 300
 aView
 
+/*
+ The soft left and right alignment constraints are activated to confine the gray subview within the bounds of the black view.
+ */
+let graySubviewBoundaryRestraint = Restraint(aView)
+    .alignItems([graySubview], to: [.softLeft, .softRight], of: blackSubview)
 graySubviewBoundaryRestraint.activate()
 aView
 
+// Undo the previous constant change
+grayWidthConstraint.constant = 100
+
 /*:
+ ## Alignment Offsets and Insets
+ 
  Alignment can be offset or inset using the `offset` and `inset` methods respectively.
+ 
+ Swift gets confused when calling a method on an enum inline within a collection, so it may be required to also denote the Alignment enum.
  */
 
 aView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
 aView.backgroundColor = .white
-grayWidthConstraint.constant = 100
+
+blackSubview = UIView()
+blackSubview.backgroundColor = .black
 
 aRestraint = Restraint(aView, items: [blackSubview, graySubview])
     .alignItems([blackSubview], to: [Alignment.top.offset(40),
