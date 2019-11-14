db95dd0e17a81ea4616cca9f8fa1d0e5a189a870
Danny Chhay <danchhay@gmail.com>
2019-11-13 19:34:48 -0800

    Add RestraintProxy, tests, reorganize

---
+++ StraitJacketTests/RestraintProxyTests.swift
@@ -0,0 +1,54 @@
+//
+//  RestraintProxyTests.swift
+//  StraitJacketTests
+//
+//  Created by Danny Chhay on 11/13/19.
+//  Copyright © 2019 Danny Chhay. All rights reserved.
+//
+
+import XCTest
+@testable import StraitJacket
+
+class RestraintProxyTests: XCTestCase {
+    
+    let view = UIView()
+    
+    lazy var proxy = RestraintProxy(view: view)
+
+    override func setUp() {
+        proxy = RestraintProxy(view: view)
+    }
+
+    func testInitialAnchors() {
+        XCTAssert(proxy.bottomAnchor == view.bottomAnchor)
+        XCTAssert(proxy.centerXAnchor == view.centerXAnchor)
+        XCTAssert(proxy.centerYAnchor == view.centerYAnchor)
+        XCTAssert(proxy.heightAnchor == view.heightAnchor)
+        XCTAssert(proxy.leadingAnchor == view.leadingAnchor)
+        XCTAssert(proxy.leftAnchor == view.leftAnchor)
+        XCTAssert(proxy.rightAnchor == view.rightAnchor)
+        XCTAssert(proxy.topAnchor == view.topAnchor)
+        XCTAssert(proxy.trailingAnchor == view.trailingAnchor)
+        XCTAssert(proxy.widthAnchor == view.widthAnchor)
+    }
+    
+    func testProxyTopToCenterAnchor() {
+        proxy.top(is: .centerY)
+        XCTAssert(proxy.topAnchor == view.centerYAnchor)
+    }
+    
+    func testProxyBottomToCenterAnchor() {
+        proxy.bottom(is: .centerY)
+        XCTAssert(proxy.bottomAnchor == view.centerYAnchor)
+    }
+    
+    func testProxyLeadingToCenterAnchor() {
+        proxy.leading(is: .centerX)
+        XCTAssert(proxy.leadingAnchor == view.centerXAnchor)
+    }
+    
+    func testProxyTrailingToCenterAnchor() {
+        proxy.trailing(is: .centerX)
+        XCTAssert(proxy.trailingAnchor == view.centerXAnchor)
+    }
+}
